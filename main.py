4599b66b113ddd8bbd33642ae6f92a8e5a770108
asgoerni-hack <scot.goernitz@cox.net>
2025-05-16 13:10:10 -0700

    Add deepsleep for 10 min, then ping and/or transmit temp.  Remove listen for signal (so no light on/off).

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 
-import machine
-import urequests
-import os
 import time
 import config
+import json
 
 # try:
 #     import iotc
@@ -12,46 +10,9 @@ import config
 #     mip.install("")
 
 import onewire, ds18x20
-from machine import Pin
+from machine import Pin, reset, deepsleep
 
-# import mip
-# mip.install("umqtt.simple", index="https://pjgpetecodes.github.io/micropython-lib/mip/add-back-ssl-params")
-# from umqtt.simple import MQTTClient, MQTTException
 from simple3 import MQTTClient, MQTTException
-
-# Download the DigiCert Certificate Der File
-print("Downloading der file from repo")
-
-filename = "digicert.der"
-url = "https://www.petecodes.co.uk/picow-iothub/digicert.der"
-
-
-def file_exists(filename):
-    try:
-        os.stat(filename)
-        return True
-    except OSError:
-        return False
-
-
-if not file_exists(filename):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-    response = urequests.get(url, headers=headers)
-    print(f"HTTP GET request to {url} returned status code {response.status_code}")
-    if response.status_code == 200:
-        with open(filename, "wb") as file:
-            file.write(response.content)
-        print("File downloaded successfully")
-    else:
-        print("Failed to download file")
-        print(response.status_code)
-        print("Response content:", response.content)
-        time.sleep(5)
-        machine.reset()
-    response.close()
-else:
-    print("File already exists")
 
 # Configure GPIO
 led = Pin(2, Pin.OUT)
@@ -69,25 +30,16 @@ clientid = config.CLIENTID
 user_name = config.USER_NAME
 passw = config.PASSW
 topic_pub = config.TOPIC_PUB
+sensor_pub_topic = config.SENSOR_PUB_TOPIC
 subscribe_topic = config.SUBSCRIBE_TOPIC
 # topic_msg = 'hello'
 port_no = 1883
 ssl_enable = False
 
 time_last = time.time()
-time_last_temp = time.time()
+time_last_temp = time.time()-20000 # so publishes on start-up
 
 def mqtt_connect():
-    certificate_path = "digicert.der"
-    print('Loading Digicert Certificate')
-    with open(certificate_path, 'rb') as f:
-        cert = f.read()
-    print('Obtained Digicert Certificate')
-    sslparams = {'cadata': cert}
-
-    import ssl
-    context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-    context.verify_mode = ssl.CERT_NONE
 
     client = MQTTClient(client_id=clientid, server=hostname, port=port_no, user=user_name, password=passw,
                         keepalive=3600, ssl=ssl_enable) #, ssl_params=sslparams) # ssl=context)
@@ -96,12 +48,10 @@ def mqtt_connect():
     print('Connected to IoT Hub MQTT Broker')
     return client
 
-
 def reconnect():
     print('Failed to connect to the MQTT Broker. Reconnecting...')
     time.sleep(5)
     machine.reset()
-
 
 def callback_handler(topic, message_receive):
     print("Received message")
@@ -116,35 +66,48 @@ def callback_handler(topic, message_receive):
 
 try:
     client = mqtt_connect()
-    client.set_callback(callback_handler)
-    client.subscribe(topic=subscribe_topic)
+    # client.set_callback(callback_handler)
+    # client.subscribe(topic=subscribe_topic)
 except OSError as e:
     print(e)
     reconnect()
 
+time.sleep(10) # give time to upload new version before going into deepsleep
+
 while True:
 
-    client.check_msg()
-    time.sleep(1)
+    # client.check_msg()
+    # time.sleep(1)
+    deepsleep(600000) # 600 seconds (in ms)
 
-    if time.time()-time_last_temp>1800: #30 min
+    if time.time()-time_last_temp>1799: #30 min - 1 sec
         ds_sensor.convert_temp() # needed to generate temp from input pin
         time.sleep_ms(750) # allow time for sensor to generate temp
-        for rom in roms:
-            print(f"Temp = {ds_sensor.read_temp(rom)*1.8+32} degF")
+        temp = ds_sensor.read_temp(roms[0])*1.8+32
+        payload = {'sensorID':'PoolTemp',
+                   'location':'Pool',
+                   'data':[
+                       {'dataType':'Temperature',
+                        'reading':temp}
+                          ]
+                   }
+        print(f"Publishing Temp of {temp} degF on topic: {sensor_pub_topic}")
+        client.publish(sensor_pub_topic,json.dumps(payload),qos=0)
         time_last_temp=time.time()
 
+    # if time.time()-time_last>600:
+    #     print("sending ping for keep-alive")
+    #     client.ping()
+    #
+    #     time_last=time.time()
+    # no longer need to check time_last since doing timed deepsleep between keep-alive...
+    print("sending ping for keep-alive")
+    client.ping()
 
-    if time.time()-time_last>600:
-        print("sending ping for keep-alive")
-        client.ping()
-
-        time_last=time.time()
-
-    if respond: #button.value():
-        # pass
-        print(f"sending message of {led.value()} on topic: {topic_pub}")
-        client.publish(topic_pub, str(led.value())) #topic_msg)
-        respond = False
-    else:
-        pass
+    # if respond: #button.value():
+    #     # pass
+    #     print(f"sending message of {led.value()} on topic: {topic_pub}")
+    #     client.publish(topic_pub, str(led.value())) #topic_msg)
+    #     respond = False
+    # else:
+    #     pass
